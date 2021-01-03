bda4462aeebdc340b2ae6e9d1c5ac2336329c56e
Joel Petracci <jpetracci@gmail.com>
2021-01-02 20:57:56 -0800

    drawing sprite

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use bevy::{prelude::*, render::pass::ClearColor};
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_resource(ClearColor(Color::rgba(0.1, 0.1, 0.1, 0.5)))
+        .add_startup_system(setup.system())
+        .run();
+}
+
+fn setup(
+    commands: &mut Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    _asset_server: Res<AssetServer>,
+) {
+    commands
+        .spawn(Camera2dBundle::default())
+        .spawn(SpriteBundle {
+            material: materials.add(Color::RED.into()),
+            transform: Transform::default(),
+            sprite: Sprite::new(Vec2::new(10.0, 10.0)),
+            ..Default::default()
+        });
 }
