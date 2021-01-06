e626531993c0abdae4262523168f71cee424fb62
Joel Petracci <jpetracci@gmail.com>
2021-01-05 23:29:33 -0800

    added some mouse clicking

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,10 @@
-use bevy::{prelude::*, render::pass::ClearColor};
+use bevy::{
+    input::{mouse::MouseButtonInput, ElementState},
+    prelude::*,
+    render::pass::ClearColor,
+    window::CursorMoved,
+};
 use rand::Rng;
-
-fn main() {
-    App::build()
-        .add_plugins(DefaultPlugins)
-        .add_resource(FireworkTimer(Timer::from_seconds(1.0, true)))
-        .add_resource(ClearColor(Color::rgb(0.1, 0.1, 0.1)))
-        .add_startup_system(setup.system())
-        .add_system(firework_propellant.system())
-        .add_system(explode.system())
-        .add_system(launcher.system())
-        .run();
-}
 
 struct Firework {
     start: Vec3,
@@ -23,6 +16,28 @@ struct Materials {
 }
 
 struct FireworkTimer(Timer);
+
+struct MousePos(Vec2);
+
+#[derive(Default)]
+struct State {
+    mouse_button_event_reader: EventReader<MouseButtonInput>,
+    cursor_moved_event_reader: EventReader<CursorMoved>,
+}
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_resource(FireworkTimer(Timer::from_seconds(1.0, true)))
+        .add_resource(ClearColor(Color::rgb(0.1, 0.1, 0.1)))
+        .add_resource(MousePos(Vec2::new(0.0, 0.0)))
+        .add_startup_system(setup.system())
+        .add_system(firework_propellant.system())
+        .add_system(explode.system())
+        .add_system(launcher.system())
+        .add_system(mouse_movement_detector.system())
+        .run();
+}
 
 fn setup(
     commands: &mut Commands,
@@ -43,6 +58,22 @@ fn setup(
     commands
         .spawn(Camera2dBundle::default())
         .insert_resource(mat_vec);
+}
+
+// need to save the mouse position manually
+fn mouse_movement_detector(
+    mut mouse_pos: ResMut<MousePos>,
+    mut state: Local<State>,
+    windows: Res<Windows>,
+    cursor_events: Res<Events<CursorMoved>>,
+) {
+    for event in state.cursor_moved_event_reader.iter(&cursor_events) {
+        let window = windows.get(event.id).unwrap();
+        // game coords are 0 0 in middle of window
+        // mouse coords are 0 0 in bottom left of winodw
+        mouse_pos.0.x = event.position.x - (window.width() as f32) / 2.0;
+        mouse_pos.0.y = event.position.y - (window.height() as f32) / 2.0;
+    }
 }
 
 fn firework_propellant(time: Res<Time>, mut query: Query<(&mut Firework, &mut Transform)>) {
@@ -69,14 +100,15 @@ fn explode(
         // check if firework rocket reached desired hieght
         // TODO: should be random after certain height, or where mouse clicked
         if firework.time_in_flight > 3.0 {
-            println!("DESPAWN");
+            let mut rng = rand::thread_rng();
             // save the current position
             // remove firework rocket
             commands.despawn(entity);
 
             // setup firework explosion
             // TODO: just a different color for now to debug
-            let mat = materials.mats[0].clone_weak();
+            let mat_index = rng.gen_range(0..(materials.mats.len()));
+            let mat = materials.mats[mat_index].clone_weak();
 
             // spawn an exploded firework
             // TODO: for now just create a large sprite at current position
@@ -95,12 +127,41 @@ fn launcher(
     commands: &mut Commands,
     materials: Res<Materials>,
     time: Res<Time>,
+    mut state: Local<State>,
+    mouse_pos: Res<MousePos>,
+    mouse_button_events: Res<Events<MouseButtonInput>>,
     mut timer: ResMut<FireworkTimer>,
 ) {
+    // spawn from mouse click
+    for event in state.mouse_button_event_reader.iter(&mouse_button_events) {
+        if event.button == MouseButton::Left && event.state == ElementState::Released {
+            println!("{:?}", mouse_pos.0);
+            // TODO: duplicating code... really...
+            let mut rng = rand::thread_rng();
+            let t_x: f32 = mouse_pos.0.x;
+            let t_y: f32 = mouse_pos.0.y;
+            let s_x: f32 = rng.gen_range(-50.0..50.0);
+
+            commands
+                .spawn(SpriteBundle {
+                    material: materials.mats[1].clone_weak(),
+                    transform: Transform::from_translation(Vec3::new(0.0, -200.0, 0.0)),
+                    sprite: Sprite::new(Vec2::new(5.0, 5.0)),
+                    ..Default::default()
+                })
+                .with(Firework {
+                    start: Vec3::new(s_x, -200.0, 0.0),
+                    target: Vec3::new(t_x, t_y, 0.0),
+                    time_in_flight: 0.0,
+                });
+        }
+    }
+
+    // spawn randomly
     if timer.0.tick(time.delta_seconds()).finished() {
         let mut rng = rand::thread_rng();
         let t_x: f32 = rng.gen_range(-400.0..400.0);
-        let t_y: f32 = rng.gen_range(200.0..300.0);
+        let t_y: f32 = rng.gen_range(110.0..300.0);
         let s_x: f32 = rng.gen_range(-50.0..50.0);
 
         // create firework projectile
@@ -119,7 +180,7 @@ fn launcher(
             });
 
         // reset launch timer to new value
-        let rnd_time = rng.gen_range(2..6);
+        let rnd_time = rng.gen_range(1..3);
         timer.0.set_duration(rnd_time as f32);
     }
 }
